c87e44cbbfb0206b361a3776fe493feb13545094
kennyzhan9 <kennyzhan9@gmail.com>
2018-07-10 11:05:04 -0400

    updated ReadMe; housekeeping on code

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
 # Test λour Memery! Game
 
-A simple photo matching memory game. The objective is to click through a series of tiles to reveal matching item pairs. Find all the pairs before the timer hits zero. Enjoy!
+###A simple and challenging photo matching memory game. The objective is to click through a series of tiles to reveal matching item pairs.
+##Each item is unique and non-repeating! (See sceenshot below)
+###Your job is to find all of the matching pairs before the timer hits zero. Enjoy!
+
+#Play Test Your Memery!
+##https://kennyzhan9.github.io/Test-Your-Memery-Game/
+
+##Screenshot:
+###![Game Screenshot](/assets/Memery.png)
+
+##Framework Used:
+###JavaScript, CSS, HTML
+
+##Motivation:
+###I built this as my first published web application as a student at General Assembly. This project was completed almost entirely through JavaScript and CSS.
+
+##Features:
+###Traditional memory matching games hinges on pattern recognition and memory recall.
+###Test-Your-Memery! adds a twist whereby pattern recognition is hindered by none-repeating item pairs that invokes reference association. Combine it all with some humor, this game will challenge your working memory in three ways!
+
+##Credits:
+###Here are some resources I found that helped with game logic planning:
+###https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/
+###https://codepen.io/natewiley/pen/HBrbL
+
+###--
+###Created by Kendrick Zhang.
+
+###General Assembly Student
+###WDI Unit One Project
 
 
---
-Created by Kendrick Zhang.
-
-General Assembly Student
-WDI Unit One Project
--- main.js
+++ main.js
@@ -22,114 +22,6 @@ const memes = [
     {name: 'life', image: 'assets/life.jpg', type: 'coding'},
 ];
 
-
-function gameTitle() {
-    //create game title
-    const divTitle = document.querySelector('#title');
-    const title = document.createElement("p");
-    title.classList.add('gameTitle');
-    title.innerHTML = `Test &lambda;our Memery!`;
-    divTitle.appendChild(title);
-}
-gameTitle();
-
-function gameMessage() {
-    const divMessage = document.querySelector('#message');
-    const message = document.createElement("ul");
-    message.classList.add('gameMessage');
-    message.innerHTML = `<li style="color:royalblue; font-size:22pt;">Memery Hints:</li>
-                            <li>Kimye</li>
-                            <li>Dr. Phil Of Shit</li>
-                            <li>BaCardi</li>
-                            <li>Peanut Butter Jelly Time!</li>
-                            <li>The Era of Dope & Grope</li>
-                            <li>Caffeine & Code</li>
-                            <li>Trump's fantasy</li>
-                            <li>Children's Favorite Snack</li>
-                            <li>Kim Jung Un's Hair Idol</li>
-                            <li>Programmer's Dilemma</li>`;
-    divMessage.appendChild(message);
-}
-gameMessage();
-
-function startScreen() {
-    const startMessage = document.createElement("div");
-    startMessage.classList.add('startScreen');
-    startMessage.innerHTML = `<div class="startScreenContent">
-                                <h1 class="modalText" style="font-family:'Wendy One',sans-serif;">Test λour Memery!</h1>
-                                <p class="modalText" style="font-size:22pt;">A simple and challenging memory matching game!</p>
-                                <p class="modalText" style="font-size:22pt;">Objective: Click each tile to reveal its content.</p>
-                                <p class="modalText" style="font-size:22pt;">Each tile is unique, but there's a matching association per two photos.</p>
-                                <p class="modalText" style="font-size:22pt;">(Check the hints on the left!)</p>
-                                <button id="hideModal">Play Now</button>
-                                </div>`
-    grid.appendChild(startMessage);
-    document.querySelector('.startScreen').style.display = 'block';
-    
-    startMessage.addEventListener('click', (e) => {
-        startMessage.style.display = 'none';
-        gameTimer();
-    });
-}
-
-function loseScreen() {
-    const loseMessage = document.createElement("div");
-    loseMessage.classList.add('loseScreen');
-    loseMessage.innerHTML = `<div class="loseScreenContent">
-                                <h1 class="loseModalText" style="font-family:'Wendy One',sans-serif;">Timer Has Expired</h1>
-                                <p class="loseModalText" style="font-size:22pt;">Try Again?</p>
-                                <button id="hideLoseModal">New Game</button>
-                                </div>`
-    grid.appendChild(loseMessage);
-    document.querySelector('.loseScreen').style.display = 'block';
-    
-    loseMessage.addEventListener('click', (e) => {
-        loseMessage.style.display = 'none';
-        location.reload();
-    });
-}
-
-function winScreen() {
-    const winMessage = document.createElement("div");
-    winMessage.classList.add('winScreen');
-    winMessage.innerHTML = `<div class="winScreenContent">
-                                <h1 class="winModalText" style="font-family:'Wendy One',sans-serif;">WINNER WINNER CHICKEN DINNER</h1>
-                                <p class="winModalText" style="font-size:22pt;">Try Again?</p>
-                                <button id="hideWinModal">New Game</button>
-                                </div>`
-    grid.appendChild(winMessage);
-    document.querySelector('.winScreen').style.display = 'block';
-    
-    winMessage.addEventListener('click', (e) => {
-        winMessage.style.display = 'none';
-        location.reload();
-    });
-
-}
-
-function gameTimer() {
-    const divTimer = document.querySelector('#timer');
-    const timer = document.createElement("h1");
-    timer.classList.add('gameTimer');
-    timer.innerHTML = `0:45`;
-    divTimer.appendChild(timer);
-
-    let timeLeft = 45;
-    let gameTime = document.querySelector('.gameTimer');
-    let countDown = setInterval(counting, 1000);
-    
-    function counting() {
-        if (timeLeft === 0 && keepCount !== 10 && all.length !== 20) {
-            clearInterval(countDown);
-            loseScreen();
-        }
-        else {
-            timeLeft -= 1;
-            gameTime.innerHTML = "0:" + timeLeft;
-        }
-    }
-}
-
 //selectors
 const grid = document.querySelector("#gameGrid");
 const all = document.querySelectorAll('.matched');
@@ -238,5 +130,112 @@ function checkWin() {
     }
     else {
         return result;
+    }
+}
+
+function gameTitle() {
+    //create game title
+    const divTitle = document.querySelector('#title');
+    const title = document.createElement("p");
+    title.classList.add('gameTitle');
+    title.innerHTML = `Test &lambda;our Memery!`;
+    divTitle.appendChild(title);
+}
+gameTitle();
+
+function gameMessage() {
+    const divMessage = document.querySelector('#message');
+    const message = document.createElement("ul");
+    message.classList.add('gameMessage');
+    message.innerHTML = `<li style="color:royalblue; font-size:22pt;">Memery Hints:</li>
+                            <li>Kimye</li>
+                            <li>Dr. Phil Of Shit</li>
+                            <li>BaCardi</li>
+                            <li>Peanut Butter Jelly Time!</li>
+                            <li>The Era of Dope & Grope</li>
+                            <li>Caffeine & Code</li>
+                            <li>Trump's fantasy</li>
+                            <li>Children's Favorite Snack</li>
+                            <li>Kim Jung Un's Hair Idol</li>
+                            <li>Programmer's Dilemma</li>`;
+    divMessage.appendChild(message);
+}
+gameMessage();
+
+function startScreen() {
+    const startMessage = document.createElement("div");
+    startMessage.classList.add('startScreen');
+    startMessage.innerHTML = `<div class="startScreenContent">
+                                <h1 class="modalText" style="font-family:'Wendy One',sans-serif;">Test λour Memery!</h1>
+                                <p class="modalText" style="font-size:22pt;">A simple and challenging memory matching game!</p>
+                                <p class="modalText" style="font-size:22pt;">Objective: Click each tile to reveal its content.</p>
+                                <p class="modalText" style="font-size:22pt;">Each tile is unique, but there's a matching association per two photos.</p>
+                                <p class="modalText" style="font-size:22pt;">(Check the hints on the left!)</p>
+                                <button id="hideModal">Play Now</button>
+                                </div>`
+    grid.appendChild(startMessage);
+    document.querySelector('.startScreen').style.display = 'block';
+    
+    startMessage.addEventListener('click', (e) => {
+        startMessage.style.display = 'none';
+        gameTimer();
+    });
+}
+
+function loseScreen() {
+    const loseMessage = document.createElement("div");
+    loseMessage.classList.add('loseScreen');
+    loseMessage.innerHTML = `<div class="loseScreenContent">
+                                <h1 class="loseModalText" style="font-family:'Wendy One',sans-serif;">Timer Has Expired</h1>
+                                <p class="loseModalText" style="font-size:22pt;">Try Again?</p>
+                                <button id="hideLoseModal">New Game</button>
+                                </div>`
+    grid.appendChild(loseMessage);
+    document.querySelector('.loseScreen').style.display = 'block';
+    
+    loseMessage.addEventListener('click', (e) => {
+        loseMessage.style.display = 'none';
+        location.reload();
+    });
+}
+
+function winScreen() {
+    const winMessage = document.createElement("div");
+    winMessage.classList.add('winScreen');
+    winMessage.innerHTML = `<div class="winScreenContent">
+                                <h1 class="winModalText" style="font-family:'Wendy One',sans-serif;">WINNER WINNER CHICKEN DINNER</h1>
+                                <p class="winModalText" style="font-size:22pt;">Try Again?</p>
+                                <button id="hideWinModal">New Game</button>
+                                </div>`
+    grid.appendChild(winMessage);
+    document.querySelector('.winScreen').style.display = 'block';
+    
+    winMessage.addEventListener('click', (e) => {
+        winMessage.style.display = 'none';
+        location.reload();
+    });
+
+}
+
+function gameTimer() {
+    const divTimer = document.querySelector('#timer');
+    const timer = document.createElement("h1");
+    timer.classList.add('gameTimer');
+    timer.innerHTML = `1:00`;
+    divTimer.appendChild(timer);
+
+    let timeLeft = 60;
+    let gameTime = document.querySelector('.gameTimer');
+    let countDown = setInterval(counting, 1000);
+    
+    function counting() {
+        if (timeLeft === 0 && keepCount !== 10 && all.length !== 20) {
+            clearInterval(countDown);
+            loseScreen();
+        }
+        else {
+            timeLeft -= 1;
+            gameTime.innerHTML = "0:" + timeLeft;
+        }
     }
 }
